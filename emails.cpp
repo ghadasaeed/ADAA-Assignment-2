27972ef0104bd66f9293a63d5d643e89eeb20b11
Terence <tankahchee00@gmail.com>
2020-12-30 17:08:58 +0800

    added heap, emails now will be stored into a dynamic array

---
+++ emails.cpp
@@ -0,0 +1,87 @@
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+//char array to make the data
+static const char pool[] =
+"ABCDEFGHIJKLMNOPQRSTUVWYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789";
+
+static const char pool3[] =
+"ABCDEFGHIJKLMNOPQRSTUVWYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789";
+
+static const char pool2[] =
+"ABCDEFGHIJKLMNOPQRSTUVWYZ"
+"abcdefghijklmnopqrstuvwxyz";
+
+static const string pool4[3] = {"com" ,"net" ,"org"};
+
+string* emails = NULL;
+
+//the size of the array
+int poolSize = sizeof(pool)-1;
+int pool2Size = sizeof(pool2)-1;
+int pool3Size = sizeof(pool3)-1;
+int pool4Size = 3;
+//random index from the array
+char getRandomChar(const char *pool0 , int poolSize0)
+{
+    return pool0[rand() % poolSize0];
+}
+string getRandomChar4()
+{
+    return pool4[rand() % pool4Size];
+}
+
+int main(int argc, char *argv[])
+{
+    emails = new string [500000];
+    while(true)
+    {
+        int passLength = 5;
+        int numberOfEmails;
+        srand (time(0));  // Init random number generator.
+        string pass;
+        string pass2;
+        string pass3;
+        string pass4;
+       
+        cout << "How many Emails you want ? ";
+        cin >> numberOfEmails;
+
+        auto start = chrono::system_clock::now();
+
+        for (int j = 0; j<numberOfEmails;j++)
+        {
+            for (int i = 0; i< passLength; i++)
+            {
+                pass += getRandomChar(pool , poolSize);
+                pass2 += getRandomChar(pool2 , pool2Size);
+                pass3 += getRandomChar(pool3 , pool3Size);
+            }
+            pass4 += getRandomChar4();
+            emails[j] = pass + "." + pass3 + "@" + pass2 + "." +pass4;
+            
+            pass = "";// to empty for the next one
+            pass2 = "";
+            pass3 = "";
+            pass4 = "";
+        }
+
+        for(int i = 0; i < numberOfEmails; i++){
+            cout << emails[i] << endl;
+        }
+        auto end = chrono::system_clock::now();
+        chrono::duration<double> duration = end - start;
+        cout << "Duration: " << duration.count() << "s\n";
+        findASCII(emails[0]);
+    }
+
+    system("PAUSE");
+    delete[] emails;
+    return EXIT_SUCCESS;
+}
